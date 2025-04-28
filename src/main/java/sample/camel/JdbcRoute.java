b0d63bdb6e11ac4affa2d289626d164a617a8668
Mitch Trachtenberg <mjtrac@gmail.com>
2025-04-28 15:37:44 -0700

    cleanup

--- src/main/java/sample/camel/JdbcRoute.java
+++ src/main/java/sample/camel/JdbcRoute.java
@@ -8,11 +8,12 @@ public class JdbcRoute extends RouteBuilder {
 
         @Override
         public void configure() throws Exception {
+	    /*
             from("direct:insert")
 		.routeId("insertMessageToDB")
-                .setBody(simple("INSERT INTO mytable (name, value) VALUES ('${header.name}', '${header.value}')"))
+                .setBody(simple("INSERT INTO mytable (route, name, value) VALUES ('${header.camelRoute}','${header.name}', '${header.value}')"))
                 .to("jdbc:dataSource");
-
+	    */
             from("direct:select")
 		.routeId("selectMessageFromDB")
                 .setBody(simple("SELECT * FROM mytable WHERE name = '${header.name}'"))
@@ -21,8 +22,9 @@ public class JdbcRoute extends RouteBuilder {
 
             from("direct:createTable")
 		.routeId("createDB")
-                .setBody(simple("CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), value VARCHAR(4096))"))
-                .to("jdbc:dataSource");
+                //.setBody(simple("CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(255), value VARCHAR(4096))"))
+                .setBody(simple("CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY AUTOINCREMENT,name VARCHAR(255),value VARCHAR(4096),created_at DATETIME DEFAULT CURRENT_TIMESTAMP,route VARCHAR(255)"))
+		.to("jdbc:dataSource");
 
             from("direct:init")
 		.routeId("initRoute")
