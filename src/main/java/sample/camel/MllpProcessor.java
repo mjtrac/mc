4388942e30b637644b7a051c98fdc41e5d326a67
Mitch Trachtenberg <mjtrac@gmail.com>
2025-05-05 12:39:12 -0700

    adding package line to files that predated package

--- src/main/java/sample/camel/MllpProcessor.java
+++ src/main/java/sample/camel/MllpProcessor.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package sample.camel;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.ProducerTemplate;  
